02ea03aea1d87124b7a60e48bb44292a40e8636e
enginewang <engine74396@gmail.com>
2019-05-13 10:01:07 +0800

    update(5.13)

---
+++ Plugin/SRombauts-UE4GitPlugin-310040f/Source/GitSourceControl/GitSourceControl.Build.cs
@@ -0,0 +1,34 @@
+// Copyright (c) 2014-2019 Sebastien Rombauts (sebastien.rombauts@gmail.com)
+//
+// Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+// or copy at http://opensource.org/licenses/MIT)
+
+using UnrealBuildTool;
+
+public class GitSourceControl : ModuleRules
+{
+	public GitSourceControl(ReadOnlyTargetRules Target) : base(Target)
+	{
+		// Enable the Include-What-You-Use (IWYU) UE4.15 policy (see https://docs.unrealengine.com/en-us/Programming/UnrealBuildSystem/IWYUReferenceGuide)
+		// "Shared PCHs may be used if an explicit private PCH is not set through PrivatePCHHeaderFile. In either case, none of the source files manually include a module PCH, and should include a matching header instead."
+		bEnforceIWYU = true;
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+		PrivatePCHHeaderFile = "Private/GitSourceControlPrivatePCH.h";
+
+		PrivateDependencyModuleNames.AddRange(
+			new string[] {
+				"Core",
+				"CoreUObject",
+				"Slate",
+				"SlateCore",
+				"InputCore",
+				"DesktopWidgets",
+				"EditorStyle",
+				"UnrealEd",
+				"SourceControl",
+				"LevelEditor",
+				"Projects",
+			}
+		);
+	}
+}

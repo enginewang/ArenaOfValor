c93f7aa4487f50f9954612768cc4f31b96066014
Freakx <>
2019-05-13 19:34:58 +0800

    test

---
+++ Plugin/SRombauts-UE4GitPlugin-310040f/Source/GitSourceControl/Private/GitSourceControlPrivatePCH.h
@@ -0,0 +1,15 @@
+// Copyright (c) 2014-2018 Sebastien Rombauts (sebastien.rombauts@gmail.com)
+//
+// Distributed under the MIT License (MIT) (See accompanying file LICENSE.txt
+// or copy at http://opensource.org/licenses/MIT)
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ISourceControlModule.h"
+#include "ISourceControlOperation.h"
+#include "ISourceControlProvider.h"
+#include "ISourceControlRevision.h"
+#include "ISourceControlState.h"
+#include "Misc/Paths.h"
+#include "Modules/ModuleManager.h"
